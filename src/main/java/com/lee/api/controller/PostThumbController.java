8214a9b10c73219e9aa97b3f33af0df44d469833
Lee <happierlee29@gmail.com>
2023-04-24 13:10:44 +0800

    完成API签名认证和客户端SDK的开发

---
+++ src/main/java/com/lee/api/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.lee.api.controller;
+
+import com.lee.api.common.BaseResponse;
+import com.lee.api.common.ErrorCode;
+import com.lee.api.common.ResultUtils;
+import com.lee.api.model.dto.postthumb.PostThumbAddRequest;
+import com.lee.api.model.entity.User;
+import com.lee.api.service.PostThumbService;
+import com.lee.api.exception.BusinessException;
+import com.lee.api.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
